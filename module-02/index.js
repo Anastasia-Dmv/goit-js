4a618f563bd0265e87fd0da3657b58c8280f12a5
Anastasia-Dmv <Vesta50000@gmail.com>
2020-06-19 15:27:26 +0300

    2m finished

--- module-02/index.js
+++ module-02/index.js
@@ -1,39 +1,37 @@
 
 
 // //===========задание 1=========///
-
-
-//  const logItems = function(array){
+ const logItems = function(array){
      
-//   for (let i = 0; i < array.length ; i += 1 ){
+  for (let i = 0; i < array.length ; i += 1 ){
         
-//             console.log(`${i+1} - ${array[i]}`);
-//         }
-//         return;
-// } ;
+            console.log(`${i+1} - ${array[i]}`);
+        }
+        return;
+} ;
 
-//  logItems(['Mango', 'Poly', 'Ajax']);
-//  logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']); 
-//  logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+ logItems(['Mango', 'Poly', 'Ajax']);
+ logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']); 
+ logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
-// //===========задание 2=========///
-// let row, quantity, price;
-// const calculateEngravingPrice = function(message, pricePerWord){
+//===========задание 2=========///
+let row, quantity, price;
+const calculateEngravingPrice = function(message, pricePerWord){
 
-//     row = message.split(` `);
-//     quantity = row.length;
-//     price = pricePerWord * quantity;
+    row = message.split(` `);
+    quantity = row.length;
+    price = pricePerWord * quantity;
 
-//     console.log(row);
-//     console.log(quantity);
-//     console.log(price);
+    console.log(row);
+    console.log(quantity);
+    console.log(price);
 
-// }
+}
 
-// calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus,', 10);
-// calculateEngravingPrice( 'Proin sociis natoque et magnis parturient montes mus', 20);
-// calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40);
-// calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20);
+calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus,', 10);
+calculateEngravingPrice( 'Proin sociis natoque et magnis parturient montes mus', 20);
+calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40);
+calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20);
 
 //===========задание 3=========///
 // let dividedString, oneOfString, numberOfLetter, eachWordDivided, word, someword; let i ;
@@ -43,34 +41,30 @@
 //    dividedString = string.split(` `);
 //    console.log(dividedString);
 
-//     word = dividedString[0];
-//     console.log(word);
+//     // word = dividedString[0];
+//     // console.log(word);
 
 //     // numberOfLetter = word.length;
 //     // console.log(numberOfLetter);
 
-//       let longestWord = ` `;
-//       console.log(longestWord);
+//       let longestWord = [0];
+//       // console.log(longestWord);
 
 //       for (let i = 0; i < dividedString.length; i += 1){
 
-//          console.log(`items[i],`, dividedString[i].length);
-//          console.log(`longest word, `, longestWord.length);
+//          console.log(`word ${[i]},`, dividedString[i], `=` ,dividedString[i].length);
+//          console.log(`longest word, `, longestWord);
 
-//          if (dividedString[i].length < longestWord.length ){
-//              longestWord = word;
+//          if (dividedString[i].length > longestWord.length ){
+//              longestWord = dividedString[i];
 //          }
 //    }
 //    return longestWord ;
-   
-
 // }
 
 
-//  console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
-
-//  findLongestWord('Google do a roll'); // 'Google'
-
+// findLongestWord('The quick brown fox jumped over the lazy dog'); // 'jumped'
+// findLongestWord('Google do a roll'); // 'Google'
 // findLongestWord('May the force be with you'); // 'force'
 
 //===========задание 4=========///
@@ -80,7 +74,6 @@
 //     console.log(string.length);
     
 //     if ( string.length <= 40){
-
 //         return string
 //     }
 //     else if( string.length > 40) {
@@ -108,8 +101,6 @@
 //     return false;
 //   }
    
-
-
 // }
 // console.log(checkForSpam('Latest technology news')); // false
 // console.log(checkForSpam('JavaScript weekly newsletter')); // false
@@ -117,34 +108,71 @@
 // console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
 
 //===========задание 6=========///
-let  input, num;
-const numbers =[];
-let total = 0;
+// let  input, num;
+// const numbers =[];
+// let total = 0;
+
+// do {
+//   input = prompt (`Enter number`);
+//   num = Number(input);
+//   numbers.push(Number(num));
+
+//   // if(input === null  || isNaN(input)){ // продолжает складывать только числа, несмотря на ввод  других знаков, , без вывода Alert;
+//   //   numbers.pop();
+//   //   console.log(numbers);
+//   // }
+//   if( input === null){
+//     numbers.pop();
+//     console.log(numbers);
+//   }
+//   if( isNaN(input)){
+//     numbers.pop();
+//     alert(`It's not a number, let's try again`);
+//   }
+// } while (input !==null ); 
+
+// const getResult = function (){
+//     for (const number of numbers){
+//       total += number ;
+//     }
+//     console.log(`total, `, total);
+// };
+
+// getResult(numbers);
+
+//===========задание 7=========///
 
 
-do {
-  input = prompt (`Enter number`);
-  num = Number(input);
-  numbers.push(Number(num));
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-  if(input === null){
-    numbers.pop();
-    console.log(numbers);
-  }
+// const isLoginValid = function(login) {
+//       if (login.length >= 4 && login.length <= 16){
+//         return true;
+//       }
+//       return false;
+//  };
 
-} while (input !==null); 
+//  const isLoginUnique = function(allLogins, login) {
+//   if (!allLogins.includes(login)){
+//     return true;
+//   } else false;
+//    };
+ 
+// const addLogin = function(allLogins, login) {
+//   if ( !isLoginValid(login)) {
+//     alert(`Ошибка! Логин должен быть от 4 до 16 символов`);
 
-console.log(numbers);
-
-
-
-const getResult = function (){
-  
-    for (const number of numbers){
-      total += number ;
-    }
-    console.log(`total, `, total);
-} ;
-
-getResult(numbers);
+//   } else if (!isLoginUnique(allLogins, login)){
+//     alert(`Такой логин уже используется!`);
+//   } else {
+//    logins.push(login);
+//    console.log(logins);
+//    console.log(`Логин успешно добавлен!`);
+//  }
+// };
+ 
+//  addLogin(logins, 'Ajax'); // 'Логин успешно добавлен!'
+//  addLogin(logins, 'robotGoogles'); // 'Такой логин уже используется!'
+//  addLogin(logins, 'Zod'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+//  addLogin(logins, 'jqueryisextremelyfast'); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
