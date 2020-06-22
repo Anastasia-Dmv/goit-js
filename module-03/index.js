810681987e2887b3d7c98f789d47cb930fc7bf60
Anastasia-Dmv <Vesta50000@gmail.com>
2020-06-22 16:04:25 +0300

    3module + 7task done

--- module-03/index.js
+++ module-03/index.js
@@ -1,5 +1,3 @@
-
-
 // // =========Number 1 ===============
 // const user = {
 //     name: 'Mango',
@@ -34,22 +32,22 @@
 // countProps({ name: 'Mango', age: 2 }); // 2  
 // countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }); // 3
 
- // =========Number 3 ===============
+// =========Number 3 ===============
 
 //  const findBestEmployee = function(employees) {
 //     console.log(employees);
-    
+
 //     let maxQuantity = 0;
- 
+
 //      const keys = Object.keys(employees);
 //      // console.log(keys);
 //      const values = Object.values(employees);
 //      // console.log(values);
-      
+
 //      for (const value of values){
 //          if (value > maxQuantity){
 //              maxQuantity  = value ;
-             
+
 //           }
 //      }
 //      for (const key in employees){
@@ -58,8 +56,8 @@
 //          }
 //      } 
 //    };
-   
-   
+
+
 //    console.log(
 //      findBestEmployee({
 //        ann: 29,
@@ -68,7 +66,7 @@
 //        lorence: 99,
 //      }),
 //    ); // lorence
-   
+
 //    console.log(
 //      findBestEmployee({
 //        poly: 12,
@@ -76,7 +74,7 @@
 //        ajax: 4,
 //      }),
 //    ); // mango
-   
+
 //    console.log(
 //      findBestEmployee({
 //        lux: 147,
@@ -85,8 +83,8 @@
 //        chelsy: 38,
 //      }),
 //    ); // lux
- 
- 
+
+
 // =========Number 4 ===============
 
 
@@ -99,10 +97,10 @@
 
 //      } return total;
 //   };
-  
+
 
 //   console.log(countTotalSalary({})); // 0
-  
+
 //   console.log(
 //     countTotalSalary({
 //       mango: 100,
@@ -110,7 +108,7 @@
 //       alfred: 80,
 //     }),
 //   ); // 330
-  
+
 //   console.log(
 //     countTotalSalary({
 //       kiwi: 200,
@@ -119,57 +117,197 @@
 //     }),
 //   ); 
 
-  // =========Number 5 ===============
+// =========Number 5 ===============
 //   const products = [
 //     { name: 'Радар', price: 1300, quantity: 4 },
 //     { name: 'Сканер', price: 2700, quantity: 3 },
 //     { name: 'Дроид', price: 400, quantity: 7 },
 //     { name: 'Захват', price: 1200, quantity: 2 },
 //   ];
-  
+
 //   const getAllPropValues = function(arr, prop) {
 //     let newPropList = [];
-    
+
 //     for (const oneInArr of arr ){
 //     const  value = oneInArr[prop] ;
-   
+
 //     newPropList.push(value) ;
 
 //         if ( !value ){
 //             newPropList.pop(value );
 //         }
-        
+
 //     } console.log(newPropList);
 //   };
-   
+
 // getAllPropValues(products, 'name'); // ['Радар', 'Сканер', 'Дроид', 'Захват']  
 // getAllPropValues(products, 'quantity'); // [4, 3, 7, 2] 
 // getAllPropValues(products, 'category'); // []
 
 // =========Number 6 ===============
-const products = [
-    { name: 'Радар', price: 1300, quantity: 4 },
-    { name: 'Сканер', price: 2700, quantity: 3 },
-    { name: 'Дроид', price: 400, quantity: 7 },
-    { name: 'Захват', price: 1200, quantity: 2 },
-  ];
-  
-  const calculateTotalPrice = function(allProducts, productName) {
-    
-    let totalPrice = 0;
-    
-    for (const product of allProducts){
-        //console.log(product);
-        if (product.name === productName ){
-            totalPrice = product.price * product.quantity;
-           return totalPrice; 
-        } 
+// const products = [{
+//     name: 'Радар',
+//     price: 1300,
+//     quantity: 4
+//   },
+//   {
+//     name: 'Сканер',
+//     price: 2700,
+//     quantity: 3
+//   },
+//   {
+//     name: 'Дроид',
+//     price: 400,
+//     quantity: 7
+//   },
+//   {
+//     name: 'Захват',
+//     price: 1200,
+//     quantity: 2
+//   },
+// ];
+
+// const calculateTotalPrice = function (allProducts, productName) {
+
+//   let totalPrice = 0;
+
+//   for (const product of allProducts) {
+//     //console.log(product);
+//     if (product.name === productName) {
+//       totalPrice = product.price * product.quantity;
+//       return totalPrice;
+//     }
+//   }
+// };
+// console.log(calculateTotalPrice(products, 'Радар')); // 5200
+// console.log(calculateTotalPrice(products, 'Дроид')); // 2800
+
+
+// ========= Number 7  ===============
+
+// const Transaction = {
+//   DEPOSIT: 'deposit',
+//   WITHDRAW: 'withdraw',
+// };
+// const account = {
+//   id: 0,
+//   balance: 0,
+//   transactions: [],
+//   createTransaction(amount, type) {
+//     this.id += 1;
+//     return {
+//       id: this.id,
+//       amount,
+//       type,
+//     }
+//   },
+//   deposit(amount) {
+//     this.balance += amount;
+//     const newTransaction = this.createTransaction(amount, Transaction.DEPOSIT);
+//     this.transactions.push(newTransaction);
+//     return this.balance;
+//   },
+
+//   withdraw(amount) {
+//     this.balance -= amount;
+//     const newTransaction = this.createTransaction(amount, Transaction.WITHDRAW);
+//     this.transactions.push(newTransaction);
+//     return this.balance;
+//   },
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getTransactionDetails(id) {
+//     for (let i = 0; i < this.transactions.length; i += 1) {
+//       if (this.transactions[i].id === id) {
+//         return this.transactions[i];
+//       }
+//     }
+
+//   },
+//   getTransactionTotal(type) {
+
+//     let total = 0;
+//     for (let i = 0; i < this.transactions.length; i += 1) {
+//       if (this.transactions[i].type === type) {
+//         total += this.transactions[i].amount;
+//       }
+//     }
+//     return total;
+//   },
+// };
+
+
+// console.log(account.deposit(400));
+// console.log(account.deposit(500));
+// console.log(account.deposit(400));
+// console.log(account.deposit(500));
+// console.log(account.withdraw(400));
+// console.log(account.withdraw(200));
+// console.log(account.withdraw(100));
+
+// console.log(account.getTransactionDetails(5));
+// console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+// console.log(account.getBalance());
+// console.table(account.transactions);
+
+// ========= Number 78 ===============
+
+const register = {
+  modules: [{
+      module1: {
+        homeTaskCount: 3,
+        doneTasks: 2
+      }
+    },
+    {
+      module2: {
+        homeTaskCount: 5,
+        doneTasks: 3
+      }
+    }, {
+      module3: {
+        homeTaskCount: 3,
+        doneTasks: 2
+      }
+    },
+    {
+      module4: {
+        homeTaskCount: 5,
+        doneTasks: 0
+      }
+    }, {
+      module5: {
+        homeTaskCount: 3,
+        doneTasks: 0
+      }
+    },
+    {
+      module6: {
+        homeTaskCount: 7,
+        doneTasks: 0
+      }
+    }
+  ],
+  currentModule: 3,
+  score: 0, //percentages
+  // посчитать какой процент выполнения курса у студента от общей программы курса,
+  // посчитать текцщий процент выполнения ДЗ студентом исходя из того, что пройдено всего 3 модуля курса
+  // предусмотреть возможность добавления и удаления из/в курс новых модулей
+  // предусмотреть возможность добавления и изменения новых заданий в конкретный модуль
+  getTotalPercentage() {
+
+  },
+  getStudentDonePercentage() {
+
+  },
+  addNewModule(module) {
+
+  },
+  removeModule(module) {
+
+  },
+  changeTaskInModule(module, task) {
+
   }
-};
-  console.log(calculateTotalPrice(products, 'Радар')); // 5200
-  console.log(calculateTotalPrice(products, 'Дроид')); // 2800
-
-
-  // =========Number 7 ===============
-
- 
+}
