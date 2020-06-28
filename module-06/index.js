15c52520ef67a362affefeb1bacaa95eb71e8e84
Anastasia-Dmv <Vesta50000@gmail.com>
2020-06-28 18:04:34 +0300

    6m

--- module-06/index.js
+++ module-06/index.js
@@ -1,68 +1,70 @@
-import {default as users} from './users.js'
+import {
+  default as users
+} from './users.js'
 
- console.table(users)
+console.table(users)
 
 // ======================N1============
 
- const  getUserNames = users =>  users.map(user => user.name);
- console.log(getUserNames(users));
+const getUserNames = users => users.map(user => user.name);
+console.log(getUserNames(users));
 
- // ======================N2============
-const getUsersWithEyeColor = (users, color) => 
- users.filter(user => user.eyeColor === color);
- console.log(getUsersWithEyeColor(users, 'blue'));
-  // ======================N3============
-  const getUsersWithGender = (users, gender) => users
+// ======================N2============
+const getUsersWithEyeColor = (users, color) =>
+  users.filter(user => user.eyeColor === color);
+console.log(getUsersWithEyeColor(users, 'blue'));
+// ======================N3============
+const getUsersWithGender = (users, gender) => users
   .filter(user => user.gender === gender)
   .map(user => user.name);
-  console.log(getUsersWithGender(users, 'male'));
-  // ======================N4============
-  const getInactiveUsers = (users) => users
+console.log(getUsersWithGender(users, 'male'));
+// ======================N4============
+const getInactiveUsers = (users) => users
   .filter(user => !user.isActive) // получили  массив объектов 
-  .map(user => user.name);     // получили массив имён
-  console.log(getInactiveUsers(users));
-  // ======================N5============
-  const gerUserWithEmail = (users, email) => users
+  .map(user => user.name); // получили массив имён
+console.log(getInactiveUsers(users));
+// ======================N5============
+const gerUserWithEmail = (users, email) => users
   .find(user => user.email === email);
-  console.log(gerUserWithEmail(users, 'shereeanthony@kog.com'));
-  // ======================N6============
+console.log(gerUserWithEmail(users, 'shereeanthony@kog.com'));
+// ======================N6============
 
 const getUserWithAge = (users, min, max) => users
-.filter(user => (user.age >= min && user.age <= max));
-console.log(getUserWithAge(users , 20, 30));
+  .filter(user => (user.age >= min && user.age <= max));
+console.log(getUserWithAge(users, 20, 30));
 console.log(getUserWithAge(users, 30, 40));
 
 
 // ======================N7============
 const calculateTotalBalance = users => users
-.reduce((totalBalance, user) => totalBalance + user.balance, 0);
+  .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 console.log(calculateTotalBalance(users));
 
 // ======================N8============
 const getUsersWithFriend = (users, FriendName) => users
 
-.filter(user => user.friends.some(name => name  === FriendName))
-.map(user =>user.name);
-console.log (getUsersWithFriend(users, 'Briana Decker'));
-console.log (getUsersWithFriend(users, 'Goldie Gentry'));
+  .filter(user => user.friends.some(name => name === FriendName))
+  .map(user => user.name);
+console.log(getUsersWithFriend(users, 'Briana Decker'));
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
 
 // ======================N9============
 
 const getNamesSortedByFriendsCount = users => users
-.sort((prevUser, nextUser) => prevUser.friends.length - nextUser.friends.length)
-.map(user => user.name);
+  .sort((prevUser, nextUser) => prevUser.friends.length - nextUser.friends.length)
+  .map(user => user.name);
 
 console.log(getNamesSortedByFriendsCount(users));
 
 // ======================N10============
 const getSortedUniqueSkills = users => {
-const allSkills = users.reduce((skills, user) => [...skills, ...user.skills],[] );
-console.log(allSkills);
+  const allSkills = users.reduce((skills, user) => [...skills, ...user.skills], []);
+  console.log(allSkills);
 
-const unrepettedSkills = allSkills.filter((skill, index) => allSkills.indexOf(skill) === index);
-console.log(unrepettedSkills);
+  const unrepetitiveSkills = allSkills.filter((skill, index) => allSkills.indexOf(skill) === index);
+  console.log(unrepetitiveSkills);
 
-return unrepettedSkills.sort();
+  return unrepetitiveSkills.sort();
 
 };
 
@@ -73,27 +75,12 @@ console.log(getSortedUniqueSkills(users));
 // const getSortedUniqueSkills = users => {
 //   const allSkills = users.reduce((skills, user) => [...skills, ...user.skills],[] );
 //   console.log(allSkills);
-  
-//   const unrepettedSkills = allSkills.reduce((uniqueSkill, skill) => uniqueSkill.includes(skill) ? uniqueSkill : [...uniqueSkill, skill],[]);
-//   console.log(unrepettedSkills);
-  
-//   return unrepettedSkills.sort();
-  
+
+//   const unrepetitiveSkills = allSkills.reduce((uniqueSkill, skill) => uniqueSkill.includes(skill) ? uniqueSkill : [...uniqueSkill, skill],[]);
+//   console.log(unrepetitiveSkills);
+
+//   return unrepetitiveSkills.sort();
+
 //   };
-  
+
 //   console.log(getSortedUniqueSkills(users));
-
-  
-
-
-
-
-
-
-
-
-
-
-  
-
-
