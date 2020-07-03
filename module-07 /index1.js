75679411f0f34cb75ab867f2fa14ed933a9f41a1
Anastasia-Dmv <Vesta50000@gmail.com>
2020-07-03 15:45:57 +0300

    7mod

--- module-07 /index1.js
+++ module-07 /index1.js
@@ -1,49 +1,4 @@
 
-//===========N1===============
-
-// const searchNumberOfcategories = document.querySelectorAll('.item');
-// console.log(` В списке ${searchNumberOfcategories.length}  категории`);
-
-// const liItem = document.querySelector('.item')
-// // console.dir(liRef);
-
-// const headingRef = liItem.firstElementChild;
-//  console.dir(headingRef.innerText);
-// // console.dir(headingRef);
-
-// const ulInLiItem = headingRef.nextElementSibling;
-// // console.log(ulInLi);
-
-// const numberOfElements = ulInLiItem.querySelectorAll('li').length;
-// console.log(numberOfElements);
-
-//  const searchNumberOfcategories = document.querySelectorAll('.item');
-//  console.log(searchNumberOfcategories);
-// //  console.log(searchNumberOfcategories[0]);
-// //  console.log(searchNumberOfcategories[1]);
-// //  console.log(searchNumberOfcategories[2]);
-
-// // console.dir(liRef);
-//  const massiv = Array.from(searchNumberOfcategories); 
-//  console.log(massiv);
-
-// const showInfo = (item, index) => {
-// const liItem = document.querySelector('.item')
-// const headingRef = liItem.firstElementChild;
-//  console.dir(headingRef.innerText);
-//  console.dir(headingRef);
-
-// const ulInLiItem = headingRef.nextElementSibling;
-//  console.log(ulInLiItem);
-
-// const numberOfElements = ulInLiItem.querySelectorAll('li').length;
-// console.log(numberOfElements); 
-// return `Категория : ${headingRef.innerText} , кол-во элементов: ${numberOfElements}`;
-// };
-// console.log(showInfo(massiv))
-//  const result = massiv.forEach(function(element, index, array){
-//      return result;
-//  });
 
 
 //===================Number 1=============
